7e6550e1fe97d6334417f8228bde2c2d74534a7b
lauri <sphaso@gmail.com>
2014-09-21 14:46:44 +0200

    copywrite, gallery directive

--- app.js
+++ app.js
@@ -18,20 +18,76 @@ app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $ur
 		})
 		.state('gallery', {
 			url: '/gallery',
-			templateUrl: 'gallery/gallery.html',
-			controller: 'galleryController'
+			templateUrl: 'gallery/gallery.html'
+		})
+		.state('contatti', {
+			url: '/contatti',
+			templateUrl: 'contatti/contatti.html'
 		});
 }]);
+
+app.factory('imagesFactory', ['$http', function($http) {
+	var dal = {
+				getImgs: function(folder){
+					return $http({ url: './' + folder + '/images.json', cache: true, method: 'GET' });
+				}
+			};
+	return dal;
+}]);
+
+app.directive('gallery', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'galleryTemplate.html',
+		scope: {folder: "=path"},
+		controller: function($scope, imagesFactory) {
+			$scope.bigImage = '';
+			$scope.images = [];
+			imagesFactory.getImgs($scope.folder.folder).success(function(data){
+						$scope.images = data.images;
+					});
+
+			$scope.showOff = function(index){
+				$scope.bigImage = $scope.images[index];
+			};
+
+			$scope.swipeImg = function(sum) {
+				var index = $scope.images.indexOf($scope.bigImage);
+				if(index == 0 && sum == -1){
+					index = $scope.images.length;
+				}
+				else if(index == ($scope.images.length - 1) && sum == 1){
+					index = -1;
+				}
+
+				$scope.bigImage = $scope.images[index + sum];
+			};
+
+			window.onhashchange = function() {
+				window.history.forward();
+			    $scope.hideDialog();
+ 
+			    $('.modal-backdrop').remove();
+			};
+		}
+	}
+});
+
 
 app.controller('parentController', ['$scope', '$state', function($scope, $state) {
 	var stateList = [
 		"news",
 		"obiettivi",
 		"mulino",
-		"gallery"
+		"gallery",
+		"contatti"
 	];
 
 	$scope.swipeTo = function(direction) {
+		if($("#galleryModal").data('bs.modal').isShown){
+			return;
+		}
+
 		var curState = $state.$current.self.name;
 		var parentState = curState.split(".")[0];
 		var parentStateIndex = stateList.indexOf(parentState);
@@ -49,7 +105,7 @@ app.controller('parentController', ['$scope', '$state', function($scope, $state)
 				$state.go(stateList[parentStateIndex - 1]);
 			}
 		} else {
-			$state.go("obiettivi");
+			$state.go("news");
 		}
 	};
 
